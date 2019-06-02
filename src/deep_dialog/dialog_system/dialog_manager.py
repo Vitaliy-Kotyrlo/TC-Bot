bc402b5da9c0e68aa04a4868248b7f7829c7972d
Vitaliy-Kotyrlo <33519292+Vitaliy-Kotyrlo@users.noreply.github.com>
2019-06-03 02:27:11 +0300

    Add files via upload
    tg

--- src/deep_dialog/dialog_system/dialog_manager.py
+++ src/deep_dialog/dialog_system/dialog_manager.py
@@ -1,8 +1,4 @@
-"""
-Created on May 17, 2016
 
-@author: xiul, t-zalipt
-"""
 
 import json
 from . import StateTracker
@@ -22,7 +18,8 @@ class DialogManager:
         self.reward = 0
         self.episode_over = False
 
-    def initialize_episode(self):
+
+    def initialize_episode(self, bot, update):
         """ Refresh state for new dialog """
         
         self.reward = 0
@@ -32,13 +29,13 @@ class DialogManager:
         self.state_tracker.update(user_action = self.user_action)
         
         if dialog_config.run_mode < 3:
-            print ("New episode, user goal:")
-            print json.dumps(self.user.goal, indent=2)
-        self.print_function(user_action = self.user_action)
+            update.message.reply_text("New episode, user goal:")
+            update.message.reply_text(json.dumps(self.user.goal, indent=2))
+        self.print_function(bot = bot, update = update, user_action = self.user_action)
             
         self.agent.initialize_episode()
 
-    def next_turn(self, record_training_data=True):
+    def next_turn(self, bot , update, record_training_data=True):
         """ This function initiates each subsequent exchange between agent and user (agent first) """
         
         ########################################################################
@@ -53,7 +50,7 @@ class DialogManager:
         self.state_tracker.update(agent_action=self.agent_action)
         
         self.agent.add_nl_to_action(self.agent_action) # add NL to Agent Dia_Act
-        self.print_function(agent_action = self.agent_action['act_slot_response'])
+        self.print_function(bot = bot, update = update,agent_action = self.agent_action['act_slot_response'])
         
         ########################################################################
         #   CALL USER TO TAKE HER TURN
@@ -67,7 +64,7 @@ class DialogManager:
         ########################################################################
         if self.episode_over != True:
             self.state_tracker.update(user_action = self.user_action)
-            self.print_function(user_action = self.user_action)
+            self.print_function(bot = bot, update = update, user_action = self.user_action)
 
         ########################################################################
         #  Inform agent of the outcome for this timestep (s_t, a_t, r, s_{t+1}, episode_over)
@@ -99,30 +96,30 @@ class DialogManager:
         return reward
     
     
-    def print_function(self, agent_action=None, user_action=None):
+    def print_function(self, bot = None, update = None, agent_action=None, user_action=None):
         """ Print Function """
             
         if agent_action:
             if dialog_config.run_mode == 0:
                 if self.agent.__class__.__name__ != 'AgentCmd':
-                    print ("Turn %d sys: %s" % (agent_action['turn'], agent_action['nl']))
+                    update.message.reply_text ("Turn %d sys: %s" % (agent_action['turn'], agent_action['nl']))
             elif dialog_config.run_mode == 1:
                 if self.agent.__class__.__name__ != 'AgentCmd':
-                    print("Turn %d sys: %s, inform_slots: %s, request slots: %s" % (agent_action['turn'], agent_action['diaact'], agent_action['inform_slots'], agent_action['request_slots']))
+                    update.message.reply_text("Turn %d sys: %s, inform_slots: %s, request slots: %s" % (agent_action['turn'], agent_action['diaact'], agent_action['inform_slots'], agent_action['request_slots']))
             elif dialog_config.run_mode == 2: # debug mode
-                print("Turn %d sys: %s, inform_slots: %s, request slots: %s" % (agent_action['turn'], agent_action['diaact'], agent_action['inform_slots'], agent_action['request_slots']))
-                print ("Turn %d sys: %s" % (agent_action['turn'], agent_action['nl']))
+                update.message.reply_text("Turn %d sys: %s, inform_slots: %s, request slots: %s" % (agent_action['turn'], agent_action['diaact'], agent_action['inform_slots'], agent_action['request_slots']))
+                update.message.reply_text ("Turn %d sys: %s" % (agent_action['turn'], agent_action['nl']))
             
             if dialog_config.auto_suggest == 1:
-                print('(Suggested Values: %s)' % (self.state_tracker.get_suggest_slots_values(agent_action['request_slots'])))
+                update.message.reply_text('(Suggested Values: %s)' % (self.state_tracker.get_suggest_slots_values(agent_action['request_slots'])))
         elif user_action:
             if dialog_config.run_mode == 0:
-                print ("Turn %d usr: %s" % (user_action['turn'], user_action['nl']))
-            elif dialog_config.run_mode == 1: 
-                print ("Turn %s usr: %s, inform_slots: %s, request_slots: %s" % (user_action['turn'], user_action['diaact'], user_action['inform_slots'], user_action['request_slots']))
+                update.message.reply_text ("Turn %d usr: %s" % (user_action['turn'], user_action['nl']))
+            elif dialog_config.run_mode == 1:
+                update.message.reply_text ("Turn %s usr: %s, inform_slots: %s, request_slots: %s" % (user_action['turn'], user_action['diaact'], user_action['inform_slots'], user_action['request_slots']))
             elif dialog_config.run_mode == 2: # debug mode, show both
-                print ("Turn %d usr: %s, inform_slots: %s, request_slots: %s" % (user_action['turn'], user_action['diaact'], user_action['inform_slots'], user_action['request_slots']))
-                print ("Turn %d usr: %s" % (user_action['turn'], user_action['nl']))
+                update.message.reply_text ("Turn %d usr: %s, inform_slots: %s, request_slots: %s" % (user_action['turn'], user_action['diaact'], user_action['inform_slots'], user_action['request_slots']))
+                update.message.reply_text ("Turn %d usr: %s" % (user_action['turn'], user_action['nl']))
             
             if self.agent.__class__.__name__ == 'AgentCmd': # command line agent
                 user_request_slots = user_action['request_slots']
@@ -131,10 +128,10 @@ class DialogManager:
                     possible_values = self.state_tracker.get_suggest_slots_values(user_action['request_slots'])
                     for slot in possible_values.keys():
                         if len(possible_values[slot]) > 0:
-                            print('(Suggested Values: %s: %s)' % (slot, possible_values[slot]))
+                            update.message.reply_text('(Suggested Values: %s: %s)' % (slot, possible_values[slot]))
                         elif len(possible_values[slot]) == 0:
-                            print('(Suggested Values: there is no available %s)' % (slot))
+                            update.message.reply_text('(Suggested Values: there is no available %s)' % (slot))
                 else:
                     kb_results = self.state_tracker.get_current_kb_results()
-                    print ('(Number of movies in KB satisfying current constraints: %s)' % len(kb_results))
+                    update.message.reply_text ('(Number of movies in KB satisfying current constraints: %s)' % len(kb_results))
                     
